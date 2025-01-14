f2aad55f6078898ed588a01fc6c4f341acbbbaa6
m5151128 <nagayama@thedott.io>
2025-01-14 11:29:34 +0900

    メンションに反応できるよう

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,14 +1,31 @@
-const { App } = require("@slack/bolt");
+import { App, ExpressReceiver } from "@slack/bolt";
+import dotenv from "dotenv";
 
-// ボットトークンと Signing Secret を使ってアプリを初期化します
+// 環境変数をロード
+dotenv.config();
+
+// Slack Boltアプリの設定
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-(async () => {
-  // アプリを起動します
-  await app.start(process.env.PORT || 3000);
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET || "",
+});
+receiver.router.post("/slack/events", (req, res) => {
+  console.log("Received a request on /slack/events");
+  res.status(200).send("OK");
+});
 
+// メンションイベントに応答
+app.event("app_mention", async ({ event, say }) => {
+  await say(`こんにちは <@${event.user}> さん！`);
+});
+
+// アプリを起動
+(async () => {
+  const port = process.env.PORT || 3000;
+  await app.start(port);
   app.logger.info("⚡️ Bolt app is running!");
 })();
